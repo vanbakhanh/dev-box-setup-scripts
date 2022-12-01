bb12de83f417afd40cec5b3c6225827e421586a7
Van Ba Khanh <vanbakhanh@gmail.com>
2022-12-01 16:07:50 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -81,9 +81,8 @@ sudo usermod -aG docker ${USER}
 
 echo "# Install Docker Compose"
 sudo apt update
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo "# Install Git"
 sudo apt update
