bac15cf12c0a6f69974507a2bd9411691fd80af5
Van Ba Khanh <vanbakhanh@gmail.com>
2022-12-01 15:07:58 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -92,6 +92,9 @@ sudo apt install -y git
 echo "# Install NVM"
 sudo apt update
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo "# Install VS Code"
 sudo apt update
